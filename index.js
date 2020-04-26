032bd2ebabe67966eee524409c608f865d9fece2
alancphilpott <alanp1995@gmail.com>
2020-04-26 15:53:53 +0100

    Added a genres route & updated index

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const genres = require("./routes/genres");
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/genres", genres);
 
 // LISTEN ON PORT
 const port = process.env.PORT || 3000;
