74fc14b7ccd2f9c5473071494b919e2a4289120c
alancphilpott <alanp1995@gmail.com>
2020-04-26 15:56:58 +0100

    Load required packages and connect to local mongodb

---
+++ routes/genres.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose
+    .connect("mongodb://localhost/genres", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((error) => console.log(`Error: ${error}`));
