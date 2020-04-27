a1e733e515c4f385005d205c98f461f193aaf75f
alancphilpott <alanp1995@gmail.com>
2020-04-27 17:53:59 +0100

    Modelled customer

--- routes/customers.js
+++ routes/customers.js
@@ -1,5 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+const customerSchema = new mongoose.Schema({
+    isGold: {
+        type: Boolean,
+        default: false
+    },
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
 
 router.get();
 
