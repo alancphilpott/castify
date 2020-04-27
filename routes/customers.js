57a36888cc19695ff75e2f3d7df538c8b9f8c9f3
alancphilpott <alanp1995@gmail.com>
2020-04-27 18:10:03 +0100

    Aded POST PUT and DELETE customer routes

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const app = express();
 app.use(express.json());
@@ -17,7 +18,7 @@ const customerSchema = new mongoose.Schema({
         maxlength: 50
     },
     phone: {
-        type: Number,
+        type: String,
         required: true,
         minlength: 5,
         maxlength: 50
@@ -29,18 +30,57 @@ const Customer = mongoose.model("Customer", customerSchema);
 // Get All Customers
 router.get("/", async (req, res) => {
     const customers = await Customer.find().sort({ name: 1 });
+    if (customers.length === 0) res.status(404).send("No Customers Found");
+    res.send(customers);
 });
 
 // Get a Customer
-router.get();
+router.get("/:id", async (req, res) => {
+    const customer = await Customer.findById(req.params.id);
+    if (!customer) res.status(404).send("Customer ID Not Found");
+    res.send(customer);
+});
 
 // Create a Customer
-router.post();
+router.post("/", async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) {
+        return res.status(404).send(error.details[0].message);
+    }
+
+    let newCustomer = new Customer({
+        name: req.body.name,
+        isGold: req.body.isGold,
+        number: req.body.number
+    });
+
+    newCustomer = await newCustomer.save();
+    res.send(newCustomer);
+});
 
 // Update a Customer
-router.put();
+router.put("/:id", async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) {
+        return res.status(404).send(error.details[0].message);
+    }
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        isGold: req.body.isGold,
+        number: req.body.number
+    });
+
+    res.send(customer);
+});
 
 // Delete a Customer
-router.delete();
+router.delete("/:id", async (req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+    if (!customer) {
+        return res.status(404).send("Customer ID Not Found");
+    }
+    res.send(customer);
+});
 
 module.exports = router;
