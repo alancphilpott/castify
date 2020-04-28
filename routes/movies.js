96a5729cadc889f5ee2639762a9132caa60f4ac1
alancphilpott <alanp1995@gmail.com>
2020-04-28 13:04:47 +0100

    Added update route to movies API

--- routes/movies.js
+++ routes/movies.js
@@ -6,20 +6,41 @@ const app = express();
 app.use(express.json());
 
 router.get("/", async (req, res) => {
-    const movies = Movie.find().sort({ name: 1 });
+    const movies = await Movie.find().sort({ name: 1 });
     if (movies.length === 0) return res.status(404).send("No Movies Found");
     res.send(movies);
 });
 
 router.get("/:id", async (req, res) => {
-    const movie = Movie.findById(req.params.id);
+    const movie = await Movie.findById(req.params.id);
     if (!movie) return res.status(404).send("Movie Not Found");
     res.send(movie);
 });
 
-router.post();
+router.post("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+});
 
-router.put();
+router.put("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let movie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+            title: req.body.title,
+            numberInStock: req.body.numberInStock,
+            dailyRentalRate: req.body.dailyRentalRate,
+            $set: {
+                "genre.name": req.body.genre.name
+            }
+        },
+        { new: true }
+    );
+
+    res.send(movie);
+});
 
 router.delete();
 
