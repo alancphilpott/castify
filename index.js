c8f42c8d7c900e37b06863bd3d199b7850ddfaeb
alancphilpott <alanp1995@gmail.com>
2020-04-28 12:43:46 +0100

    Load movie router in index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 
 // Connect to MongoDB
 mongoose
