b70748def13504dd6f289005c921bb83ce126c28
alancphilpott <alanp1995@gmail.com>
2020-04-28 12:46:39 +0100

    Added GET service to movies API

--- routes/movies.js
+++ routes/movies.js
@@ -5,9 +5,17 @@ const { Movie, validate } = require("../models/movie");
 const app = express();
 app.use(express.json());
 
-router.get();
+router.get("/", async (req, res) => {
+    const movies = Movie.find().sort({ name: 1 });
+    if (movies.length === 0) return res.status(404).send("No Movies Found");
+    res.send(movies);
+});
 
-router.get();
+router.get("/:id", async (req, res) => {
+    const movie = Movie.findById(req.params.id);
+    if (!movie) return res.status(404).send("Movie Not Found");
+    res.send(movie);
+});
 
 router.post();
 
