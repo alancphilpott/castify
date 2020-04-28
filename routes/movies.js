dde7e13e0353010032f892b23a895e199c84aa0a
alancphilpott <alanp1995@gmail.com>
2020-04-28 12:33:54 +0100

    Created template for movies endpoint

---
+++ routes/movies.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { Movie, validate } = require("../models/movie");
+
+const app = express();
+app.use(express.json());
+
+router.get();
+
+router.get();
+
+router.post();
+
+router.put();
+
+router.delete();
+
+module.exports = router;
