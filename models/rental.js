10c96719b15e49e7d03ef96b82f6f6f69843ec2f
alancphilpott <alanp1995@gmail.com>
2020-04-29 11:03:39 +0100

    Created rental model

---
+++ models/rental.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Joi = require("Joi");
+
+const Rental = mongoose.model(
+    "Rental",
+    new mongoose.Schema({
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Customer",
+            required: true
+        },
+        movie: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Movie",
+            required: true
+        },
+        dateOut: {
+            type: Date,
+            default: Date.now,
+            required: true
+        },
+        dateIn: {
+            type: Date
+        },
+        rentalFee: {
+            type: Number,
+            min: 0
+        }
+    })
+);
+
+function validateRental(rental) {
+    const schema = {
+        customerId = Joi.string().required,
+        movieId = Joi.string().required
+    }
+    return Joi.validate(rental, schema);
+}
+
+exports.Rental = Rental;
+exports.validate = this.validateRental;
