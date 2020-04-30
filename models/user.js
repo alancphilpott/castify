380702333dd5a583853cb49e563e665e8503d413
alancphilpott <alanp1995@gmail.com>
2020-04-30 11:31:46 +0100

    Created user model

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const User = mongoose.model(
+    "User",
+    new mongoose.model({
+        name: {
+            type: String,
+            minlength: 4,
+            maxlength: 50,
+            required: true
+        },
+        email: {
+            type: String,
+            minlength: 4,
+            maxlength: 100,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            minlength: 8,
+            maxlength: 50,
+            required: true
+        }
+    })
+);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.String().min(4).max(50).required(),
+        email: Joi.String().min(4).max(100).required(),
+        password: Joi.String().min(8).max(50).required()
+    };
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
