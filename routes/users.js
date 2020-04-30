acc1516b8ed70407e76fff5274ed6217fe3f7fc9
alancphilpott <alanp1995@gmail.com>
2020-04-30 11:37:46 +0100

    Created users endpoint with a GET service

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { User, validate } = require("../models/user");
+
+router.post("/", async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(erorr.details[0].message);
+
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    await user.save();
+    res.send(newUser);
+});
+
+module.exports = router;
