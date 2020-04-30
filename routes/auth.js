d8cca21d164fa3b8346c6d9046044d86959227b1
alancphilpott <alanp1995@gmail.com>
2020-04-30 14:34:53 +0100

    Fix error handling finding user & validating login request

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,14 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
 
 router.post("/", async (req, res) => {
     const { error } = validateLogin(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send("Invalid Email or Password");
+    if (!user) return res.status(400).send("Invalid Email or Password");
 
     if (!(await bcrypt.compare(req.body.password, user.password)))
         return res.status(400).send("Invalid Email or Password");
@@ -22,7 +23,7 @@ function validateLogin(req) {
         email: Joi.string().min(6).max(100).required(),
         password: Joi.string().min(8).max(50).required()
     };
-    return Joi.validate(user, schema);
+    return Joi.validate(req, schema);
 }
 
 module.exports = router;
