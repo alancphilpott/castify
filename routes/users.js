734a795d57462d085362ba026be1f57e3e3e12ed
alancphilpott <alanp1995@gmail.com>
2020-04-30 16:26:14 +0100

    Added custom header to user registration HTTP response

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
     const { error } = validate(req.body);
@@ -15,7 +17,11 @@ router.post("/", async (req, res) => {
     user.password = await bcrypt.hash(user.password, await bcrypt.genSalt(10));
 
     await user.save();
-    res.send(_.pick(user, ["_id", "name", "email"]));
+
+    const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+    res.header("x-auth-token", token).send(
+        _.pick(user, ["_id", "name", "email"])
+    );
 });
 
 module.exports = router;
