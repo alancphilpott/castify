664a2223f54293d72b158469617fd202bcaef126
alancphilpott <alanp1995@gmail.com>
2020-04-30 11:58:41 +0100

    fixed typo

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { User, validate } = require("../models/user");
 
 router.post("/", async (req, res) => {
     const { error } = validate(req.body);
-    if (error) return res.status(400).send(erorr.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
     const user = await User.findOne({ email: req.body.email });
     if (user)
