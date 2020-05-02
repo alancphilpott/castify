6a13e52edabfbe503617f4e8324036ad43328840
alancphilpott <alanp1995@gmail.com>
2020-05-02 19:25:46 +0100

    Moved jwt and config module loading to user

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 
 router.post("/", async (req, res) => {
     const { error } = validateLogin(req.body);
