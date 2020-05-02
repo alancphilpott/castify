77456e1626cc8a38fa779f0ff2dbb80231f266cb
alancphilpott <alanp1995@gmail.com>
2020-05-02 19:16:19 +0100

    Initialise user schema independently

--- models/user.js
+++ models/user.js
@@ -1,30 +1,29 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const User = mongoose.model(
-    "User",
-    new mongoose.Schema({
-        name: {
-            type: String,
-            minlength: 4,
-            maxlength: 50,
-            required: true
-        },
-        email: {
-            type: String,
-            minlength: 6,
-            maxlength: 100,
-            unique: true,
-            required: true
-        },
-        password: {
-            type: String,
-            minlength: 8,
-            maxlength: 1024,
-            required: true
-        }
-    })
-);
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 4,
+        maxlength: 50,
+        required: true
+    },
+    email: {
+        type: String,
+        minlength: 6,
+        maxlength: 100,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        maxlength: 1024,
+        required: true
+    }
+});
+
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
     const schema = {
