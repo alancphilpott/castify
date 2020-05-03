be84c0b0bdb06317c3ac9b3726c75685ef92e456
alancphilpott <alanp1995@gmail.com>
2020-05-03 11:03:32 +0100

    GET User Request

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,8 @@ const config = require("config");
 
 function auth(req, res, next) {
     const token = req.header("x-auth-token");
-    if (!token) return res.status(401).send("Access Denied. No Token Provided");
+    if (!token)
+        return res.status(401).send("Access Denied. No Token Provided.");
 
     try {
         const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@ const express = require("express");
 const router = express.Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
+
+router.get("/me", auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
+});
 
 router.post("/", async (req, res) => {
     const { error } = validate(req.body);
