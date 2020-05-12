71515deb4dfa12f2fa03da0b14f83103a3dc53e3
alancphilpott <alanp1995@gmail.com>
2020-05-12 16:37:14 +0100

    Move config logic to separate module

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const app = express();
 const Joi = require("joi");
-const config = require("config");
 const express = require("express");
 Joi.objectId = require("joi-objectid")(Joi);
 
 require("./startup/logging")();
+require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-// Check for JWT Secret Configuration
-if (!config.get("jwtPrivateKey")) {
-    console.error("FATAL ERROR: JWT Secret Not Defined");
-    process.exit(1);
-}
-
-// Listen on Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening On Port ${port}...`);
