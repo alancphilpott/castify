78f10507defbde36c801c3cd6bc5b20800b44e48
alancphilpott <alanp1995@gmail.com>
2020-05-12 16:44:59 +0100

    Move validation logic to separate module. Seperation of concerns complete

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const app = express();
-const Joi = require("joi");
 const express = require("express");
-Joi.objectId = require("joi-objectid")(Joi);
 
 require("./startup/logging")();
 require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
