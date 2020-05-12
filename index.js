8b2e9bafbb3060066c82ecaa897546c886880bb3
alancphilpott <alanp1995@gmail.com>
2020-05-12 15:47:45 +0100

    Move routes logic to separate module. SOC.

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
 const winston = require("winston");
 require("winston-mongodb");
+
+const app = express();
+require("./startup/routes")(app);
 
 // Handling Node Process Exceptions
 winston.exceptions.handle(new winston.transports.File({ filename: "UE.log" }));
@@ -35,18 +30,6 @@ if (!config.get("jwtPrivateKey")) {
     console.error("FATAL ERROR: JWT Secret Not Defined");
     process.exit(1);
 }
-
-// Middleware
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-// Error Middleware
-app.use(error);
 
 // Connect to MongoDB
 mongoose.set("useCreateIndex", true);
