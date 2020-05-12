0172d229c0815fb96bfeb04d58f2bb74d8d7c1c5
alancphilpott <alanp1995@gmail.com>
2020-05-12 16:14:59 +0100

    Move logging logic to separate module

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-const express = require("express");
-const winston = require("winston");
-require("winston-mongodb");
-
 const app = express();
+const Joi = require("joi");
+const config = require("config");
+const express = require("express");
+Joi.objectId = require("joi-objectid")(Joi);
+
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-// Handling Node Process Exceptions
-winston.exceptions.handle(new winston.transports.File({ filename: "UE.log" }));
-process.on("unhandledRejection", (ex) => {
-    throw ex;
-});
-
-// Add Winston Logging Transports
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-    new winston.transports.MongoDB({
-        db: "mongodb://localhost/castify",
-        level: "error",
-        options: { useUnifiedTopology: true }
-    })
-);
 
 // Check for JWT Secret Configuration
 if (!config.get("jwtPrivateKey")) {
