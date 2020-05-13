3707f9622960cbebd991a9c9a00a9e7ae4a197e3
alancphilpott <alanp1995@gmail.com>
2020-05-13 13:24:29 +0100

    Set export to a function rather than function name

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,6 @@
 const winston = require("winston");
 
-function error(error, req, res, next) {
+module.exports = function (error, req, res, next) {
     winston.error(error.message);
     res.status(500).send("Something Went Wrong Mate");
-}
-
-module.exports = error;
+};
