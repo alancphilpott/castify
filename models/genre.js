840cb2267026c0d973c8fb675c590b34ff3e7014
alancphilpott <alanp1995@gmail.com>
2020-05-13 13:27:30 +0100

    Updated Joi validation syntax to match updated Joi version

--- models/genre.js
+++ models/genre.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
 
 const genreSchema = new mongoose.Schema({
     name: {
@@ -12,10 +12,10 @@ const genreSchema = new mongoose.Schema({
 const Genre = mongoose.model("Genre", genreSchema);
 
 function validateGenre(genre) {
-    const schema = {
+    const schema = Joi.object({
         name: Joi.string().min(5).max(50).required()
-    };
-    return Joi.validate(genre, schema);
+    });
+    return schema.validate(genre);
 }
 
 exports.Genre = Genre;
--- models/rental.js
+++ models/rental.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
 
 const Rental = mongoose.model(
     "Rental",
@@ -30,11 +30,11 @@ const Rental = mongoose.model(
 );
 
 function validateRental(rental) {
-    const schema = {
+    const schema = Joi.object({
         customerId: Joi.objectId().required(),
         movieId: Joi.objectId().required()
-    };
-    return Joi.validate(rental, schema);
+    });
+    return schema.validate(rental);
 }
 
 exports.Rental = Rental;
