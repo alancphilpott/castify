9356053c4ec686365b51e21d23b45086ebe9c83f
alancphilpott <alanp1995@gmail.com>
2020-05-13 13:23:25 +0100

    Set export to a function rather than function name

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,4 @@
-function checkAdmin(req, res, next) {
+module.exports = function (req, res, next) {
     if (!req.user.isAdmin) return res.status(403).send("Access Denied.");
     next();
-}
-
-module.exports = checkAdmin;
+};
