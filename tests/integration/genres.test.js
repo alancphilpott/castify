541704cf83ccb07545e0f27a3025ab0d02614c54
alancphilpott <alanp1995@gmail.com>
2020-05-17 14:14:01 +0100

    Populate genre collection with test docs. Remove docs after each test.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,18 +1,28 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
 let server;
 
 describe("/api/genres", () => {
     beforeEach(() => {
         server = require("../../index");
     });
-    afterEach(() => {
+    afterEach(async () => {
         server.close();
+        await Genre.remove({});
     });
 
     describe("GET /", () => {
         it("should return all genres", async () => {
+            await Genre.collection.insertMany([
+                { name: "Genre 1" },
+                { name: "Genre 2" }
+            ]);
+
             const res = await request(server).get("/api/genres");
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some((g) => g.name == "Genre 1")).toBeTruthy();
+            expect(res.body.some((g) => g.name == "Genre 2")).toBeTruthy();
         });
     });
 });
