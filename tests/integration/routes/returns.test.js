2b0139465bc89a7a0043b4d4144b1e45036df7a8
alancphilpott <alanp1995@gmail.com>
2020-05-19 17:30:58 +0100

    created int test file for new returns endpoint

---
+++ tests/integration/routes/returns.test.js
@@ -0,0 +1,47 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const { Movie } = require("../../../models/movie");
+const { Rental } = require("../../../models/rental");
+const { Customer } = require("../../../models/customer");
+
+describe("/api/returns", () => {
+    const endpoint = "/api/returns/";
+
+    let server;
+    let rental;
+    let customer, customerId;
+    let movie, movieId;
+
+    beforeEach(async () => {
+        server = require("../../../index");
+
+        customerId = mongoose.Types.ObjectId();
+        customer = new Customer({
+            _id: customerId,
+            name: "1234",
+            phone: "123456"
+        });
+
+        movieId = mongoose.Types.ObjectId();
+        movie = new Movie({
+            _id: movieId,
+            title: "12345",
+            dailyRentalRate: 2
+        });
+
+        rental = new Rental({
+            customer: customer._id,
+            movie: movie._id
+        });
+        await rental.save();
+    });
+    afterEach(async () => {
+        server.close();
+        await Rental.remove({});
+    });
+
+    it("should work!", async () => {
+        const result = await Rental.findById(rental._id);
+        expect(result).not.toBeNull();
+    });
+});
