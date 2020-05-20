a1dd4b167a01c617b9448124d9a247f16413424b
alancphilpott <alanp1995@gmail.com>
2020-05-20 16:44:46 +0100

    Update rental endpoint to check for existing rental

--- routes/returns.js
+++ routes/returns.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { Rental } = require("../models/rental");
 
 router.post("/", async (req, res) => {
-    res.status(401).send("Unauthorized");
+    const token = req.header("x-auth-token");
+    if (!token) return res.status(401).send("Unauthorized");
+
+    if (!req.body.customerId) res.status(400).send("No CustomerId Provided");
+    if (!req.body.movieId) res.status(400).send("No MovieId Provided");
+
+    const rental = await Rental.findOne({
+        customerId: req.body.customerId,
+        movieId: req.body.movieId
+    });
+    if (!rental) return res.status(404).send("Rental Not Found");
 });
 
 module.exports = router;
