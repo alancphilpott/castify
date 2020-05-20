f9f2084e949554fee408b6c87a8c501f25b9e7a5
alancphilpott <alanp1995@gmail.com>
2020-05-20 17:16:29 +0100

    Update rental endpoint to check if rental already processed

--- routes/returns.js
+++ routes/returns.js
@@ -6,14 +6,16 @@ router.post("/", async (req, res) => {
     const token = req.header("x-auth-token");
     if (!token) return res.status(401).send("Unauthorized");
 
-    if (!req.body.customerId) res.status(400).send("No CustomerId Provided");
-    if (!req.body.movieId) res.status(400).send("No MovieId Provided");
+    if (!req.body.customerId)
+        return res.status(400).send("No CustomerId Provided");
+    if (!req.body.movieId) return res.status(400).send("No MovieId Provided");
 
     const rental = await Rental.findOne({
-        customerId: req.body.customerId,
-        movieId: req.body.movieId
+        customer: req.body.customerId,
+        movie: req.body.movieId
     });
     if (!rental) return res.status(404).send("Rental Not Found");
+    if (rental.dateIn) res.status(400).send("Rental Already Processed");
 });
 
 module.exports = router;
