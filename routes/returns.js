2950e81aeb4debf2a74099a470abbd11331efe7c
alancphilpott <alanp1995@gmail.com>
2020-05-22 12:40:06 +0100

    Update finding of a rental

--- routes/returns.js
+++ routes/returns.js
@@ -11,8 +11,8 @@ router.post("/", auth, async (req, res) => {
     if (!req.body.movieId) return res.status(400).send("No MovieId Provided");
 
     const rental = await Rental.findOne({
-        customer: req.body.customerId,
-        movie: req.body.movieId
+        "customer._id": req.body.customerId,
+        "movie._id": req.body.movieId
     });
     if (!rental) return res.status(404).send("Rental Not Found");
 
