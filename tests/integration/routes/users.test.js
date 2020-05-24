05a375855182b2fef9131e37c12896e9cd416bd4
alancphilpott <alanp1995@gmail.com>
2020-05-24 13:58:25 +0100

    Users route int tests created for get and post endpoint

--- tests/integration/routes/users.test.js
+++ tests/integration/routes/users.test.js
@@ -53,4 +53,79 @@ describe("/api/users", () => {
             );
         });
     });
+
+    describe("POST /", () => {
+        let payload;
+
+        const exec = () => {
+            return request(server).post(endpoint).send(payload);
+        };
+
+        beforeEach(() => {
+            payload = {
+                name: "1234",
+                email: "123456",
+                password: "12345678"
+            };
+        });
+
+        it("should return 400 if name is less than 5 characters", async () => {
+            payload.name = "";
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if name is more than 50 characters", async () => {
+            payload.name = new Array(52).join("a");
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if email is less than 6 characters", async () => {
+            payload.email = "12345";
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if email is more than 100 characters", async () => {
+            payload.email = new Array(102).join("a");
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if password is less than 8 characters", async () => {
+            payload.password = "1234567";
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if password is more than 50 characters", async () => {
+            payload.password = new Array(52).join("a");
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if user already exists", async () => {
+            const user = new User({
+                name: "1234",
+                email: "123456",
+                password: "12345678"
+            });
+            await user.save();
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return auth token in response header if input is valid", async () => {
+            const res = await exec();
+            expect(res.header).toHaveProperty("x-auth-token");
+        });
+
+        it("should return user if input is valid", async () => {
+            const res = await exec();
+            expect(Object.keys(res.body)).toEqual(
+                expect.arrayContaining(["_id", "name", "email"])
+            );
+        });
+    });
 });
