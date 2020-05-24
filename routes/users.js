89e44f41ca4beb625738690175cf15c8f414dbf6
alancphilpott <alanp1995@gmail.com>
2020-05-24 14:01:36 +0100

    Users POST endpoint req validation now handled using middleware

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,15 @@ const router = express.Router();
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const auth = require("../middleware/auth");
-const { User, validate } = require("../models/user");
+const validate = require("../middleware/validate");
+const { User, validateUser } = require("../models/user");
 
 router.get("/me", auth, async (req, res) => {
     const user = await User.findById(req.user._id).select("-password");
     res.send(user);
 });
 
-router.post("/", async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
+router.post("/", validate(validateUser), async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send("User Already Exists");
 
