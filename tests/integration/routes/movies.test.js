dcc9ebb966849720c8dc063b245971a3e0090747
alancphilpott <alanp1995@gmail.com>
2020-05-25 17:15:37 +0100

    Create Movies GET/:id endpoint integration tests

--- tests/integration/routes/movies.test.js
+++ tests/integration/routes/movies.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const mongoose = require("mongoose");
 const { Movie } = require("../../../models/movie");
+const { Genre } = require("../../../models/genre");
 
 describe("/api/movies", () => {
     const endpoint = "/api/movies/";
@@ -13,6 +14,7 @@ describe("/api/movies", () => {
     afterEach(async () => {
         await server.close();
         await Movie.deleteMany({});
+        await Genre.deleteMany({});
     });
 
     describe("GET /", () => {
@@ -33,6 +35,37 @@ describe("/api/movies", () => {
             expect(res.body.length).toBe(2);
             expect(res.body.some((m) => m.title == "Movie 1")).toBeTruthy();
             expect(res.body.some((m) => m.title == "Movie 2")).toBeTruthy();
+        });
+    });
+
+    describe("GET /:id", () => {
+        let genre, movie;
+
+        beforeEach(async () => {
+            genre = new Genre({ name: "Genre 1" });
+            await genre.save();
+
+            movie = new Movie({ title: "Movie 1", genre: genre._id });
+            await movie.save();
+        });
+
+        console.log(movie);
+
+        it("should return a movie if valid id is passed", async () => {
+            const res = await request(server).get(endpoint + movie._id);
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty("title", movie.title);
+        });
+
+        it("should return 400 if invalid id is passed", async () => {
+            const res = await request(server).get(endpoint + "1");
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 404 if no genre with given id exists", async () => {
+            const id = mongoose.Types.ObjectId();
+            const res = await request(server).get(endpoint + id);
+            expect(res.status).toBe(404);
         });
     });
 });
