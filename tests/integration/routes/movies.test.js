2875a46e80be86e030291c0124dbe8211b46aae1
alancphilpott <alanp1995@gmail.com>
2020-05-25 17:57:02 +0100

    Add Movies POST endpoint integration tests for 400 and 404 errors

--- tests/integration/routes/movies.test.js
+++ tests/integration/routes/movies.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const mongoose = require("mongoose");
 const { Movie } = require("../../../models/movie");
 const { Genre } = require("../../../models/genre");
+const { User } = require("../../../models/user");
 
 describe("/api/movies", () => {
     const endpoint = "/api/movies/";
@@ -63,6 +64,59 @@ describe("/api/movies", () => {
         it("should return 404 if no genre with given id exists", async () => {
             const id = mongoose.Types.ObjectId();
             const res = await request(server).get(endpoint + id);
+            expect(res.status).toBe(404);
+        });
+    });
+
+    describe("POST /", () => {
+        let token;
+        let title;
+        let genre, genreId;
+
+        const execution = () => {
+            return request(server)
+                .post(endpoint)
+                .set("x-auth-token", token)
+                .send({ title, genreId });
+        };
+
+        beforeEach(async () => {
+            genre = new Genre({ name: "Genre 1" });
+            await genre.save();
+
+            token = new User().generateAuthToken();
+
+            title = "A Movie";
+            genreId = genre._id;
+        });
+
+        it("should return 401 if client is not logged in", async () => {
+            token = "";
+            const res = await execution();
+            expect(res.status).toBe(401);
+        });
+
+        it("should return 400 if title is less than 5 characters", async () => {
+            title = "1234";
+            const res = await execution();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if title is more than 255 characters", async () => {
+            title = new Array(257).join("a");
+            const res = await execution();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if genreId is not provided", async () => {
+            genreId = "";
+            const res = await execution();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 404 if genre with given id not found", async () => {
+            genreId = mongoose.Types.ObjectId();
+            const res = await execution();
             expect(res.status).toBe(404);
         });
     });
