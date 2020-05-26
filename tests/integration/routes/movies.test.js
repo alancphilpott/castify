ddc31e01584706970f1ee0fd38e4585749a54618
alancphilpott <alanp1995@gmail.com>
2020-05-26 12:52:55 +0100

    Movies PUT endpoint integration tests complete

--- tests/integration/routes/movies.test.js
+++ tests/integration/routes/movies.test.js
@@ -134,4 +134,86 @@ describe("/api/movies", () => {
             expect(res.body).toHaveProperty("title", "A Movie");
         });
     });
+
+    describe("PUT /", () => {
+        let token;
+        let movie, movieId, newTitle;
+        let genre, genreId;
+
+        const execution = () => {
+            return request(server)
+                .put(endpoint + movieId)
+                .set("x-auth-token", token)
+                .send({ title: newTitle, genreId });
+        };
+
+        beforeEach(async () => {
+            genre = new Genre({ name: "Genre 1" });
+            await genre.save();
+            genreId = genre._id;
+
+            movie = new Movie({
+                title: "A Movie",
+                genre: {
+                    _id: genreId,
+                    name: genre.name
+                }
+            });
+            await movie.save();
+            movieId = movie._id;
+
+            token = new User().generateAuthToken();
+            newTitle = "A New Movie";
+        });
+
+        it("should return 401 if client is not logged in", async () => {
+            token = "";
+            const res = await execution();
+            expect(res.status).toBe(401);
+        });
+
+        it("should return 400 if title is less than 5 characters", async () => {
+            newTitle = "1234";
+            const res = await execution();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if title is more than 255 characters", async () => {
+            newTitle = new Array(257).join("a");
+            const res = await execution();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 400 if genreId is not provided", async () => {
+            genreId = "";
+            const res = await execution();
+            expect(res.status).toBe(400);
+        });
+
+        it("should return 404 if genre with given id not found", async () => {
+            genreId = mongoose.Types.ObjectId();
+            const res = await execution();
+            expect(res.status).toBe(404);
+        });
+
+        it("should return 404 if movie with given id not found", async () => {
+            movieId = mongoose.Types.ObjectId();
+            const res = await execution();
+            expect(res.status).toBe(404);
+        });
+
+        it("should update the movie if it is valid", async () => {
+            await execution();
+            const updatedMovie = await Movie.find({ name: "A New Movie" });
+            expect(movie).not.toBeNull();
+        });
+
+        it("should return valid movie in body of response", async () => {
+            const res = await execution();
+            expect(Object.keys(res.body)).toEqual(
+                expect.arrayContaining(["_id", "title", "genre"])
+            );
+            expect(res.body).toHaveProperty("title", "A New Movie");
+        });
+    });
 });
