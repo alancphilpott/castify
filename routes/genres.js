e091f5cfcdb6918bf9a82b40c2ac436b097cb9bf
alancphilpott <alanp1995@gmail.com>
2020-08-03 22:17:16 +0100

    Removed unused import

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const validate = require("../middleware/validate");
